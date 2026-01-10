67a69fc874921bf06bbf18932bd363174f78be1a
Paweł Łakomiec <pawellakomiec95@gmail.com>
2026-01-10 15:05:46 +0100

    init 3

--- cyan-chroma/astro.config.mjs
+++ cyan-chroma/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://s14094.github.io',
-    base: '/katra/',
+    base: '/katra/cyan-chroma/',
 });
